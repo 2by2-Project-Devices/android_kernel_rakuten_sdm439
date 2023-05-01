17818f91629a4ad65ce3da0641f59af4ae783d43
me-cafebabe <me.cafebabe@gmail.com>
2023-05-01 22:16:51 +0800

    camera_v2: eeprom: Return 0 when probing eeprom again
    Change-Id: I3017a21195629202b8b7ac4499098ddb4f9a32d6

--- drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
+++ drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -718,7 +718,7 @@ static int msm_eeprom_config(struct msm_eeprom_ctrl_t *e_ctrl,
 		if (e_ctrl->userspace_probe == 0) {
 			pr_err("%s:%d Eeprom already probed at kernel boot",
 				__func__, __LINE__);
-			rc = -EINVAL;
+			rc = 0;
 			break;
 		}
 		if (e_ctrl->cal_data.num_data == 0) {
@@ -1687,7 +1687,7 @@ static int msm_eeprom_config32(struct msm_eeprom_ctrl_t *e_ctrl,
 		if (e_ctrl->userspace_probe == 0) {
 			pr_err("%s:%d Eeprom already probed at kernel boot",
 				__func__, __LINE__);
-			rc = -EINVAL;
+			rc = 0;
 			break;
 		}
 		if (e_ctrl->cal_data.num_data == 0) {
